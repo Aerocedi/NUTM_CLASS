4d2d37dba66a1e7a9367d117f5c7f9868415b34d
unknown <Peter>
2021-10-27 11:04:17 +0100

    Update

---
+++ assignment1/one.py
@@ -0,0 +1,40 @@
+
+age = 9
+print(age)
+
+money_out = 455.20
+print(money_out)
+
+light = "stop"
+print(light)
+
+first_name = "Peter"
+surname = "Abosede"
+print(first_name)
+print(surname)
+
+first_name = "Peter"
+surname = "Abosede"
+full_name= first_name +" "+surname
+print(full_name)
+
+year = "2021"
+month = "October"
+date = "26th"
+current_date = date + " " + month + " " +year
+print(current_date)
+
+year = "2021"
+month = "October"
+date = "26th"
+Birth_date = date + " " + month + " " +year
+print("Today's date is " + Birth_date)
+
+
+print (5+6+3)
+
+age = 20
+age_next_year = age + 1
+print (age_next_year)
+
+print ("My favourite movie quote is \"I'll never let you go Jack!\"." )
